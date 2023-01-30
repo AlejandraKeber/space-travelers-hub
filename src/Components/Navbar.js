d9ea0be0012f3cb544b1009c0b85f80ae22545b1
shinhyo26okazaki <shibo6937@gmail.com>
2023-01-30 15:56:49 -0400

    Add NavLinks

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav>Navbar</nav>
+    <nav>
+      <ul>
+        <li>
+        <NavLink
+          to="/missions"
+          style={({ isActive }) => (isActive ? { color: 'blue' } : { color: 'black' })}
+        >
+          Missions
+        </NavLink>
+        <NavLink
+          to="/"
+          style={({ isActive }) => (isActive ? { color: 'blue' } : { color: 'black' })}
+        >
+          Rockets
+        </NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
