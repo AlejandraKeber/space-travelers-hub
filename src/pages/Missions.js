0a98e343fbba895a3b1bb91fe44c6b9b9216e76d
shinhyo26okazaki <shibo6937@gmail.com>
2023-01-31 19:45:00 -0400

    Add style to the missions table

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import Mission from '../Components/Mission';
+import '../css/mission.css';
 
-export default function missions() {
+export default function Missions() {
+  const missions = useSelector((state) => state.missionsStore.missions);
   return (
-    <div>missions</div>
+    <table>
+      <tbody>
+        <tr className="tableHeader">
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <td />
+        </tr>
+        {
+          missions.map((mission) => (
+            <Mission
+              key={uuidv4()}
+              id={mission.missionId}
+              name={mission.missionName}
+              description={mission.description}
+            />
+          ))
+        }
+      </tbody>
+    </table>
   );
 }
