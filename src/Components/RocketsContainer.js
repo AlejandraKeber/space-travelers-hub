ab5bd34ee5cf35d33609269821236f680f421b1f
AlejandraKeber <alejandrakeber@gmail.com>
2023-01-31 17:43:25 -0500

    Display Rockets - list render

--- src/Components/RocketsContainer.js
+++ src/Components/RocketsContainer.js
@@ -1,6 +1,27 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 import { getRocket } from '../redux/rockets/rockets';
+import '../css/RocketsContainer.css';
+
+const Rocket = ({
+  name, img, description,
+}) => (
+  <div className="rocket flex">
+    <img src={img} alt="rocket" />
+    <div className="rocket-details">
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <button type="button">Reserve Rocket</button>
+    </div>
+  </div>
+);
+
+Rocket.propTypes = {
+  name: PropTypes.node.isRequired,
+  img: PropTypes.node.isRequired,
+  description: PropTypes.node.isRequired,
+};
 
 const RocketsContainer = () => {
   const rockets = useSelector((state) => state.rocket);
@@ -13,7 +34,14 @@ const RocketsContainer = () => {
   return (
     <>
       {rockets.map((rocket) => (
-        <p key={rocket.id}>{rocket.name}</p>
+        <Rocket
+          key={rocket.id}
+          id={rocket.id}
+          name={rocket.name}
+          type={rocket.type}
+          img={rocket.image}
+          description={rocket.description}
+        />
       ))}
     </>
   );
