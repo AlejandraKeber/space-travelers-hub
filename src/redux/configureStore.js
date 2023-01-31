9dca19a0aa3d5dcb3ddabd28e94744f3cf9634ca
AlejandraKeber <alejandrakeber@gmail.com>
2023-01-31 14:29:36 -0500

    Fetch Rockets data

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import missionsReducer from './missions/missions';
+import rocketReducer from './rockets/rockets';
 
 const store = configureStore({
   reducer: {
     missionsStore: missionsReducer,
+    rocket: rocketReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
