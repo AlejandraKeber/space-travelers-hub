eb90766fc26c5b13159223314e0783e7e35df51e
shinhyo26okazaki <shibo6937@gmail.com>
2023-02-02 10:57:06 -0400

    Add basic structure for My Profile page

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import '../css/Myprofile.css';
 
 export default function Myprofile() {
   return (
-    <div>My profile</div>
+    <main className="MyProfile">
+      <section className="MyMissions">
+        <h2>My Missions</h2>
+      </section>
+      <section className="MyRockets">
+        <h2>My Rockets</h2>
+      </section>
+    </main>
   );
 }
