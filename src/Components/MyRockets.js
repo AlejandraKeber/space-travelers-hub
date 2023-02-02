6a5d7d10c81d4c92e167f3b311832ae3a037b548
shinhyo26okazaki <shibo6937@gmail.com>
2023-02-02 18:28:42 -0400

    Add style css to My rockets

--- src/Components/MyRockets.js
+++ src/Components/MyRockets.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import '../css/MyRockets.css';
 
 const MyRockets = ({
   name,
 }) => (
-  <ul>
+  <ul className="MyRocketsUl">
     <li>
       {name}
     </li>
