37a1599165cd8860a4629846ca8b295d72f2f4d4
AlejandraKeber <alejandrakeber@gmail.com>
2023-02-02 19:59:54 -0500

    Update readme and unify test folder

--- src/__tests__/MyProfileMissions.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import MyProfileMissions from '../Components/MyProfileMissions';
-
-it('renders without crashing', () => {
-  const table = document.createElement('li');
-  const root = createRoot(table);
-  root.render(<MyProfileMissions />);
-});
